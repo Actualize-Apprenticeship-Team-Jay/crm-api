51838c916fa0a3361acf859714f3cfc0a15a9413
saiyencoder <jsantizo20@gmail.com>
2017-11-30 19:57:02 -0600

    WIP Added lead id to outreach table.

---
+++ db/migrate/20171201015402_add_leadid_to_outreach.rb
@@ -0,0 +1,5 @@
+class AddLeadidToOutreach < ActiveRecord::Migration[5.0]
+  def change
+    add_column :outreaches, :lead_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,7 @@
 
 ActiveRecord::Schema.define(version: 20171208003203) do
 
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -101,6 +102,13 @@ ActiveRecord::Schema.define(version: 20171208003203) do
     t.string   "auto_text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "outreaches", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "lead_id"
+
   end
 
   add_foreign_key "events", "leads"
